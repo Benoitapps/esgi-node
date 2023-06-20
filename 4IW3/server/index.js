4db29cb95d51ac3c560a7f47476f15aaa04f40a1
Karl MARQUES <kmarques@vetixy.com>
2023-06-20 15:06:49 +0200

    4IW3: validation

--- 4IW3/server/index.js
+++ 4IW3/server/index.js
@@ -3,6 +3,16 @@ const app = express();
 const GenericRouter = require("./routes/genericCRUD");
 const GenericController = require("./controllers/genericCRUD");
 const userService = require("./services/user");
+const errorHandler = require("./middlewares/errorHandler");
+
+app.use(function (req, res, next) {
+  if (["POST", "PUT", "PATCH"].includes(req.method)) {
+    if (!req.is("application/json")) {
+      return res.sendStatus(400);
+    }
+  }
+  next();
+});
 
 app.use(express.json());
 
@@ -18,5 +28,7 @@ app.get("/", (req, res) => {
 app.post("/", (req, res) => {
   res.json(req.body);
 });
+
+app.use(errorHandler);
 
 app.listen(3000, () => console.log("Server started on port 3000"));
--- 4IW3/server/services/user.js
+++ 4IW3/server/services/user.js
@@ -1,4 +1,6 @@
+const { Sequelize } = require("sequelize");
 const { User } = require("../db");
+const ValidationError = require("../errors/ValidationError");
 
 module.exports = {
   findAll: async function (criteria, options = {}) {
@@ -12,16 +14,30 @@ module.exports = {
     return User.findByPk(id);
   },
   create: async function (data) {
-    return User.create(data);
+    try {
+      return User.create(data);
+    } catch (e) {
+      if (e instanceof Sequelize.ValidationError) {
+        throw ValidationError.createFromSequelizeValidationError(e);
+      }
+      throw e;
+    }
   },
   update: async function (criteria, data) {
-    const [nb, users = []] = await User.update(data, {
-      where: criteria,
-      returning: true,
-      individualHooks: true,
-    });
-    console.log(nb, users);
-    return users;
+    try {
+      const [nb, users = []] = await User.update(data, {
+        where: criteria,
+        returning: true,
+        individualHooks: true,
+      });
+      console.log(nb, users);
+      return users;
+    } catch (e) {
+      if (e instanceof Sequelize.ValidationError) {
+        throw ValidationError.createFromSequelizeValidationError(e);
+      }
+      throw e;
+    }
   },
   remove: async function (criteria) {
     return User.destroy({
