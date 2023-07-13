116bf1286c88368387d85b208fae832c5bb199ff
Karl MARQUES <kmarques@vetixy.com>
2023-07-13 16:55:57 +0200

    4IW1: DI

---
+++ 4IW1/client/src/views/SecuredZone.vue
@@ -0,0 +1,22 @@
+<template>
+  Connected as {{ user.email }}
+  <Button title="Logout" @click="logout" />
+  <h1>User list (li)</h1>
+  <UserList />
+  <h1>User list (span)</h1>
+  <UserList>
+    <template #item>
+      <span>Custom item</span>
+    </template>
+  </UserList>
+</template>
+
+<script setup>
+import { inject } from 'vue';
+import Button from '@/components/Button.vue';
+import UserList from '@/views/UserList.vue';
+import { userKey, logoutKey } from '@/providers/AuthProviderKeys';
+
+const user = inject(userKey);
+const logout = inject(logoutKey);
+</script>
---
+++ 4IW1/client/src/views/Security.vue
@@ -0,0 +1,16 @@
+<template>
+  <h1>Login</h1>
+  <LoginForm :on-submit="login" />
+  <h1>Register</h1>
+  <UserForm :on-submit="register" />
+</template>
+
+<script setup>
+import { inject } from 'vue';
+import { loginKey, registerKey } from '@/providers/AuthProviderKeys';
+import LoginForm from '@/components/LoginForm.vue';
+import UserForm from '@/components/UserForm.vue';
+
+const login = inject(loginKey);
+const register = inject(registerKey);
+</script>
